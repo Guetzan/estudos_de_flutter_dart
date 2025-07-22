3e5811cd2c7033341da566017b9dcd938a436866
Clovis <orcsdzn@gmail.com>
2025-07-21 22:22:29 -0300

    add small refactor

--- le_petit_quizz/lib/main.dart
+++ le_petit_quizz/lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:le_petit_quizz/gradient_container.dart';
+import 'package:le_petit_quizz/quiz_title.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF0974f1),
+                Color(0xFF9fccfa),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: const QuizTitle(),
+        ),
       ),
     ),
   );
---
+++ le_petit_quizz/lib/quiz_title.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class QuizTitle extends StatelessWidget {
+  const QuizTitle({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 240,
+          ),
+
+          const SizedBox(height: 80),
+
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(220, 255, 255, 255),
+              fontSize: 24,
+            ),
+          ),
+
+          const SizedBox(height: 40),
+
+          FilledButton(
+            style: FilledButton.styleFrom(
+              foregroundColor: const Color.fromARGB(220, 255, 255, 255),
+              backgroundColor: const Color.fromARGB(122, 9, 117, 241),
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              textStyle: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            onPressed: () => {},
+            child: const Text('❯  Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
