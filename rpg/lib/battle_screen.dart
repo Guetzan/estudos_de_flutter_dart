9dc9938c6acd378376ce1fff8de37d82288ad782
Antares <orcsdzn@gmail.com>
2025-07-30 15:17:31 -0300

    implement action panel initial prototype

--- rpg/lib/battle_screen.dart
+++ rpg/lib/battle_screen.dart
@@ -15,11 +15,49 @@ class _BattleScreen extends State<BattleScreen> {
   Widget build(context) {
     return Container(
       padding: EdgeInsets.all(20),
-      child:Column(
+      child: Column(
         children: [
           SizedBox(height: 100),
 
           EnemyFrame(),
+
+          SizedBox(height: 100),
+
+          /*TODO:
+              - FIND OUT HOW TO SET RESPONSIVE WIDTHS
+              - MAKE THE ACTIONS PANEL A SEPARATED WIDGET
+              - ADD ONE MORE ROW WITH TWO MORE ACTIONS (POT, SHIELD (?))
+              - MAKE PLAYER HEALTH BAR
+          */
+          Row(
+            children: [
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(0),
+                  ),
+                  minimumSize: Size(180, 50),
+                ),
+                onPressed: () => {},
+                child: Text('ATTACK'),
+              ),
+
+              SizedBox(width: 30),
+
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(0),
+                  ),
+                  minimumSize: Size(180, 50),
+                ),
+                onPressed: () => {},
+                child: Text('DODGE'),
+              ),
+            ],
+          ),
         ],
       ),
     );
