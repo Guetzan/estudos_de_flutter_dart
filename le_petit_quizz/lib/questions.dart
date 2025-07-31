906f54c5f138e9677a581730cf0c5976d58e837b
Antares <orcsdzn@gmail.com>
2025-07-30 22:41:10 -0300

    add questions placeholder to the questions screen

--- le_petit_quizz/lib/questions.dart
+++ le_petit_quizz/lib/questions.dart
@@ -3,17 +3,38 @@ import 'package:flutter/material.dart';
 class Questions extends StatefulWidget {
   const Questions({super.key});
 
-  @override 
+  @override
   State<Questions> createState() {
     return _QuestionsState();
   }
 }
 
 class _QuestionsState extends State<Questions> {
-  @override 
+  @override
   Widget build(context) {
-    return Center(
-      child: Text("WIP"),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Question text'),
+
+          SizedBox(width: 80),
+
+          FilledButton(
+            onPressed: () => {},
+            child: Text('Alternative'),
+          ),
+          FilledButton(
+            onPressed: () => {},
+            child: Text('Alternative'),
+          ),
+          FilledButton(
+            onPressed: () => {},
+            child: Text('Alternative'),
+          ),
+        ],
+      ),
     );
   }
 }
