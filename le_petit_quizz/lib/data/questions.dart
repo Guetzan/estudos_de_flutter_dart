233e4e7d0caaa1c2559279730ab04f1abb045d0e
Antares <orcsdzn@gmail.com>
2025-07-30 22:13:11 -0300

    create questions model and data files

---
+++ le_petit_quizz/lib/data/questions.dart
@@ -0,0 +1,13 @@
+import 'package:le_petit_quizz/models/question.dart';
+
+const questions = [
+  Question(
+    'What are the main building blocks of Flutter UIs?',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+];
---
+++ le_petit_quizz/lib/models/question.dart
@@ -0,0 +1,6 @@
+class Question {
+  const Question(this.question, this.alternatives);
+
+  final String question;
+  final List<String> alternatives;
+}
